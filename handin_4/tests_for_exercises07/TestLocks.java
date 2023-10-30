ea43bc6b86ad84560a078947ad9288457a99d2cf
Matheus Valentim <75315649+mavalentim@users.noreply.github.com>
2023-10-31 00:42:08 +0100

    adds one more test and a new response

--- handin_4/tests_for_exercises07/TestLocks.java
+++ handin_4/tests_for_exercises07/TestLocks.java
@@ -86,61 +86,54 @@ public class TestLocks {
 
     }
 
-    // I got told by CHATGPT that the one above doesnt directly test the
-    // specificities of the
+    // Seq in a single thread: the reader cannot access the lock when the writer has
+    // it
+
     @Test
-    void TestWritersOptionCGPT() {
-        final ReadWriteCASLock lock = new ReadWriteCASLock();
-        final AtomicBoolean testResult = new AtomicBoolean(true);
-        final int numThreads = 5;
-        final CountDownLatch startLatch = new CountDownLatch(1);
-        final CountDownLatch finishLatch = new CountDownLatch(numThreads);
+    void TestingWriters22() {
+        // im gonna create like a shared value and two writer threads
+        Histogram1 testHist = new Histogram1(3);
+        ReadWriteCASLock myLock = new ReadWriteCASLock();
 
-        for (int i = 0; i < numThreads; i++) {
-            new Thread(() -> {
-                try {
-                    startLatch.await(); // Wait until all threads are ready to start
+        Thread t1 = new Thread(() -> {
+            if (myLock.writerTryLock()) {
 
-                    if (lock.writerTryLock()) {
-                        // Ensure that only one thread can acquire the lock
-                        for (int j = 0; j < 1000000; j++) {
-                            // Simulate some work
-                        }
-                        lock.writerUnlock();
-                    } else {
-                        testResult.set(false); // Test failed if more than one thread acquired the lock
+                if (myLock.writerTryLock()) {
+                    try {
+                        testHist.increment(2);
+                        myLock.writerUnlock();
+                    } catch (Exception e) {
+                        System.out.println("someone missed2");
+                        e.printStackTrace();
                     }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                } finally {
-                    finishLatch.countDown();
-                }
-            }).start();
-        }
 
-        startLatch.countDown(); // Start all threads simultaneously
+                }
+                try {
+                    myLock.writerTryLock();
+                    testHist.increment(1);
+                    myLock.writerUnlock();
+                } catch (Exception e) {
+                    System.out.println("someone missed");
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        t1.start();
 
         try {
-            finishLatch.await(); // Wait for all threads to finish
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            t1.join();
+        } catch (Exception e) {
+
+        } finally {
+            assertTrue(testHist.getMyset().contains("1"));
         }
 
-        if (testResult.get()) {
-            System.out.println("Non-blocking mutual exclusive lock test passed!");
-        } else {
-            System.out.println("Non-blocking mutual exclusive lock test failed!");
-        }
     }
 
-    // And finally I need a test to test wheather the readers and writers phase each
-    // other
-    // First I will check if they respect each other = 1 reader and 1 writer
     @Test
-    void TestingReadersAndWriters() {
+    void TestingReadersAndWriters22() {
 
         final CountDownLatch startLatch = new CountDownLatch(1);
         // im gonna create like a shared value and two writer threads
@@ -226,4 +219,59 @@ public class TestLocks {
         }
 
     }
+
+    // I got told by CHATGPT that the one above doesnt directly test the
+    // specificities of the
+    @Test
+    void TestWritersOptionCGPT() {
+        final ReadWriteCASLock lock = new ReadWriteCASLock();
+        final AtomicBoolean testResult = new AtomicBoolean(true);
+        final int numThreads = 5;
+        final CountDownLatch startLatch = new CountDownLatch(1);
+        final CountDownLatch finishLatch = new CountDownLatch(numThreads);
+
+        for (int i = 0; i < numThreads; i++) {
+            new Thread(() -> {
+                try {
+                    startLatch.await(); // Wait until all threads are ready to start
+
+                    if (lock.writerTryLock()) {
+                        // Ensure that only one thread can acquire the lock
+                        for (int j = 0; j < 1000000; j++) {
+                            // Simulate some work
+                        }
+                        lock.writerUnlock();
+                    } else {
+                        testResult.set(false); // Test failed if more than one thread acquired the lock
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } finally {
+                    finishLatch.countDown();
+                }
+            }).start();
+        }
+
+        startLatch.countDown(); // Start all threads simultaneously
+
+        try {
+            finishLatch.await(); // Wait for all threads to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (testResult.get()) {
+            System.out.println("Non-blocking mutual exclusive lock test passed!");
+        } else {
+            System.out.println("Non-blocking mutual exclusive lock test failed!");
+        }
+    }
+
+    // And finally I need a test to test wheather the readers and writers phase each
+    // other
+    // First I will check if they respect each other = 1 reader and 1 writer
+
 }
