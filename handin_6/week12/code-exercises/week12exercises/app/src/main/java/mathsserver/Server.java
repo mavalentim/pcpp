526b012ecccf5ec2634f936b41d584bd5e0e8f2f
Matheus Valentim <75315649+mavalentim@users.noreply.github.com>
2023-12-05 04:12:44 +0100

    Fix bug in login functionality

--- handin_6/week12/code-exercises/week12exercises/app/src/main/java/mathsserver/Server.java
+++ handin_6/week12/code-exercises/week12exercises/app/src/main/java/mathsserver/Server.java
@@ -1,5 +1,7 @@
 package mathsserver;
 
+import akka.actor.SupervisorStrategy;
+
 // Hint: The imports below may give you hints for solving the exercise.
 //       But feel free to change them.
 
@@ -8,16 +10,20 @@ import akka.actor.typed.Behavior;
 import akka.actor.typed.ChildFailed;
 import akka.actor.typed.Terminated;
 import akka.actor.typed.javadsl.*;
-
 import java.util.Queue;
+import java.util.Random;
+import java.util.Set;
+import java.util.Stack;
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.stream.IntStream;
-
+import mathsserver.Client.ClientCommand;
 import mathsserver.Task;
 import mathsserver.Task.BinaryOperation;
+import mathsserver.Worker.ComputeTask;
 import mathsserver.Worker.WorkerCommand;
 
 public class Server extends AbstractBehavior<Server.ServerCommand> {
@@ -46,14 +52,18 @@ public class Server extends AbstractBehavior<Server.ServerCommand> {
 
 	/* --- State ---------------------------------------- */
 	// To be implemented
-	int minWorkers;
-	int maxWorkers;
-
+	private final int minWorkers;
+	private final int maxWorkers;
+	private int existantWorkers; // to track the current number of workers
 	// Some sort of list to keep track of all the tasks that we have
-	ArrayList<Task> tasksToBeAssigned;
+	Stack<Task> pendingTasks;
+	Stack<ActorRef<Client.ClientCommand>> pendingClients;
 
 	// HashMap to keep track of the workers and their status
 	HashMap<ActorRef<Worker.WorkerCommand>, Boolean> available_workers;
+	Stack<ActorRef<Worker.WorkerCommand>> avWorks;
+	HashMap<ActorRef<WorkerCommand>, Task> jobsInProgress;
+	HashMap<ActorRef<WorkerCommand>, ActorRef<Client.ClientCommand>> jobsInProgress2;
 
 	/* --- Constructor ---------------------------------- */
 	// the constructor is defined here but we do not use it directly, we use the
@@ -63,15 +73,31 @@ public class Server extends AbstractBehavior<Server.ServerCommand> {
 		super(context);
 		// To be implemented
 		// Set the minimum and maximum to the local variables
-		minWorkers = minWorkers;
-		maxWorkers = maxWorkers;
-		// initialize the hashmap for tracking which workers are inactive
-		available_workers = new HashMap<>();
-		// initialize the list or whatever structure to keep track of the other tasks
-		tasksToBeAssigned = new ArrayList<>();
+		this.minWorkers = minWorkers;
+		this.maxWorkers = maxWorkers;
+		pendingTasks = new Stack<>();
+		pendingClients = new Stack<>();
+		avWorks = new Stack<>();
+		existantWorkers = 0;
+
+		jobsInProgress = new HashMap<ActorRef<Worker.WorkerCommand>, Task>();
+		jobsInProgress2 = new HashMap<ActorRef<Worker.WorkerCommand>, ActorRef<Client.ClientCommand>>();
 
 		// spawn the minimum workers and set their value as available in the hashmap
-		ActorRef<Worker.WorkerCommand> worker = getContext().spawn(Worker.create(this), "server");
+		int i = 0;
+		while (i < this.minWorkers) {
+
+			// spawn the minimum amount of workers
+			final ActorRef<Worker.WorkerCommand> worker = getContext().spawn(Worker.create(getContext().getSelf()),
+					"worker_n" + i);
+			// watch it
+			getContext().watch(worker);
+			existantWorkers++;
+			// put them in the hashmap with availability true
+			// available_workers.put(worker, true);
+			avWorks.push(worker);
+			i++;
+		}
 
 	}
 
@@ -86,22 +112,74 @@ public class Server extends AbstractBehavior<Server.ServerCommand> {
 		return newReceiveBuilder()
 				.onMessage(ComputeTasks.class, this::onComputeTasks)
 				.onMessage(WorkDone.class, this::onWorkDone)
+				.onSignal(ChildFailed.class, this::onChildFailed)
 				// To be extended
 				.build();
 	}
 
 	/* --- Handlers ------------------------------------- */
 	public Behavior<ServerCommand> onComputeTasks(ComputeTasks msg) {
-		// To be implemented
-		// The list of tasks should be appended to my local state somehow
-		msg.tasks.stream().forEach(x -> tasksToBeAssigned.add(x));
-		// spawn workers to get these tasks done
+		// How many workers are available rn
+
+		// Create a list of tasks
+		// ArrayList<Task> tasksToBeAssigned = new ArrayList<>();
+		// for each task received, put it thru the pipeline
+
+		msg.tasks.stream().forEach(task -> {
+			if (avWorks.size() > 0) { // there are idle workers
+				ActorRef<Worker.WorkerCommand> worker = avWorks.pop(); // get one from the availables
+				worker.tell(new ComputeTask(task, msg.client)); // tell it to do the task
+
+				jobsInProgress.put(worker, task);
+				jobsInProgress2.put(worker, msg.client); // add it to jobs
+															// in progress
+
+			} else if (avWorks.isEmpty() & existantWorkers < this.maxWorkers) {
+				existantWorkers++;
+				ActorRef<Worker.WorkerCommand> worker = getContext().spawn(Worker.create(getContext().getSelf()),
+						"worker_nonOG" + existantWorkers);
+				getContext().watch(worker); // watch it
+				worker.tell(new ComputeTask(task, msg.client));
+				jobsInProgress.put(worker, task);
+				jobsInProgress2.put(worker, msg.client);
+			} else {
+				pendingTasks.push(task);
+				pendingClients.push(msg.client);
+			}
+
+		});
+
+		return this;
+	}
+
+	public Behavior<ServerCommand> onChildFailed(ChildFailed signal) {
+		ActorRef<Void> actorFailed = signal.getRef();
+		System.out.println(actorFailed + " failed! :((");
+
+		// get the task AND THE CLIENT that was not finished
+
+		Task incompleteTask = jobsInProgress.remove(actorFailed);
+		ActorRef<Client.ClientCommand> incompleteClient = jobsInProgress2.remove(actorFailed);
+		Random randomgen = new Random();
+		int newnametag = randomgen.nextInt();
+		ActorRef<Worker.WorkerCommand> worker = getContext().spawn(Worker.create(getContext().getSelf()),
+				getContext().getSelf().path().name() + newnametag);
+		getContext().watch(worker);
+
+		worker.tell(new ComputeTask(incompleteTask, incompleteClient));
 
 		return this;
 	}
 
 	public Behavior<ServerCommand> onWorkDone(WorkDone msg) {
-		// To be implemented
+		/*
+		 * if (!pendingTasks.isEmpty()) {
+		 * msg.worker.tell(new ComputeTask(pendingTasks.pop(), pendingClients.pop()));
+		 * } else {
+		 * avWorks.push(msg.worker);
+		 * }
+		 */
+
 		return this;
 	}
 }
